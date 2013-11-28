2e56c7e845aa2580c34b6bcb04d404c0f6972f2a
Fabio Borella <info@pencildrummer.com>
2013-11-28 13:03:53 +0100

    Renamed PHPExcel to Excel to avoid naming conflicts - Added magic methods to forward method to internal PHPExcel instance

--- Model/Excel.php
+++ Model/Excel.php
@@ -1,6 +1,6 @@
 <?php
 
-class PhpExcel extends AppModel {
+class Excel extends AppModel {
 	
 	public $useTable = false;
 	
@@ -8,15 +8,23 @@ class PhpExcel extends AppModel {
 	
 	public function __construct($id = false, $table = false, $ds = false) {
 		parent::__construct($id, $table, $ds);
-		$loaded = App::import('Vendor', 'PHPExcel/Classes/PHPExcel');
-		if (!$loaded || !class_exist('PHPExcel'))
+		$loaded = App::import('Vendor', 'PhpExcel.PHPExcel/Classes/PHPExcel');
+		if (!$loaded || !class_exists('PHPExcel'))
 			throw new CakeException('Vendor class PHPExcel not found!');
 		$this->_PHPExcel = new PHPExcel();
+	}
+	
+	public function __call($name, $args) {
+		if (method_exists($this->_PHPExcel, $name)) {
+			return call_user_func_array(array($this->_PHPExcel, $name), $args);
+		}
 	}
 	
 	public function load($filePath) {
 		$this->_PHPExcel = PHPExcel_IOFactory::load($filePath);
 		return $this->_PHPExcel ? true : false;
 	}
+	
+	
 	
 }
